1d4a76265d31ba0cadcd42d19b24971fe7718c41
Kshitij bajpai <sanjaikumarbajpai305@gmail.com>
2025-05-12 13:35:13 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId=6473839
+let accountEmail="kshitij@google.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+
+//accountId=2
+
+accountEmail="kb@h.com"
+accountPassword="898932008"
+accountCity="bengaluru"
+let accountState;
+
+console.log(accountId);
+/*
+prefer not to use var bcoz of issue in block scope or functional scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+
+
