5b5f6968242426a20837955b7280b5d95f73c95e
Kshitij bajpai <sanjaikumarbajpai305@gmail.com>
2025-05-15 14:13:36 +0000

    conversion of datatypes

---
+++ 01_basics/03_datatypes_conversion.js
@@ -0,0 +1,28 @@
+// let  score=true
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber= Number(score)
+// console.log(typeof valueInNumber);
+// console.log( valueInNumber);
+
+//"33"=>>easily convert in string
+//"33abc"==>>NaN
+// true=>>1; false=>0
+
+// let isLoggedIn= "hitesh"
+// let booleanisLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn)
+
+
+//1=> true
+// 2=>false
+// ""=>false
+//"hitesh"=> true
+
+let someNumber = 33
+
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log( typeof stringNumber);
