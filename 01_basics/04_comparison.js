3afc5bdab8b47e1b784fd3a5ed30df949edca4b7
Kshitij bajpai <sanjaikumarbajpai305@gmail.com>
2025-05-15 14:45:05 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,21 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2">1);
+// console.log("02">1);
+
+
+console.log(null>0);
+console.log(null==0);
+console.log(null >=0);
+
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined<0);
+// the above 6 comaprison is laways avoided they are two confusing
+// == 
+console.log("2"===2);
+
