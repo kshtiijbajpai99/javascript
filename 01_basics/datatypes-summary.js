222071835f8ff8a414ed067ae45080f96fb362f7
Kshitij bajpai <sanjaikumarbajpai305@gmail.com>
2025-05-20 13:50:51 +0000

    datatypes summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,41 @@
+// primitive data types
+
+// 7 categories :  String ,number,boolean,null,undefined,symbol,bigInt
+const score=100
+const scoreValue=100.3
+
+const isLoggedIn= false
+const outsideTemp= null
+let useremail;
+
+
+const id = Symbol('123')
+const anotherId=Symbol('123')
+console.log(id=== anotherId);
+
+const bigNumber = 4895839532403247034211n
+
+
+
+// Refrence type  or non primitive
+
+// Array,objects,Functions
+
+const heros=["shaktiman","naagraj","doga"];
+{
+    name:'kshitij';
+    age:22;
+}
+
+
+const myfunction = function(){
+    console.log("hello world");
+}
+
+
+console.log(typeof myfunction)
+
+
+
+//*************null kam datatype object aata ha */
+
