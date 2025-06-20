f5f03fbc8f5bbac95db951a4f434b0edef8b472b
Kshitij bajpai <sanjaikumarbajpai305@gmail.com>
2025-06-20 18:16:14 +0000

    05_string.js

---
+++ 01_basics/05_string.js
@@ -0,0 +1,49 @@
+const name ="kshitij"
+const repocount=50
+console.log(`hello my name is ${name} and my repo count is $ {repocount}`)
+//practise more this above syntax
+const gamename = new String("kshitij")
+console.log(gamename[0])
+console.log(gamename.__proto__)
+
+
+console.log(gamename.length)
+console.log(gamename.toUpperCase())
+console.log(gamename.charAt(2));
+
+
+console.log(gamename.indexOf('t'))
+
+
+const newstring = gamename.substring(0,4)
+console.log(newstring)
+
+const anotherstring = gamename.slice(-8,4)
+console.log(anotherstring)
+
+
+const newstringone="        hitesh       "
+console.log(newstringone)
+console.log(newstringone.trim())
+const url ="http ://kshitij.com/kshitij&20bajpai"
+console.log(url.replace('%20','-'))
+
+console.log(url.includes("kshitij"))
+
+console.log(gamename.split('-'))
+
+//try to ore familiar whith strieng
+
+
+
+
+
+
+
+
+
+
+
+
+
+
